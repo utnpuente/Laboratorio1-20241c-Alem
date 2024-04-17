1c10e62930c267a893344a0327f501c4a06ff412
Alejandro Gomez Nieto <agomeznieto@outlook.es>
2024-04-17 12:36:31 -0300

    correccion de gitignore

--- Semana 02/e2.cpp
+++ Semana 02/e2.cpp
@@ -1,5 +1,5 @@
 /*
-	Hacer un programa que permita ingresar dos número enteros por teclado.
+	Hacer un programa que permita ingresar dos nï¿½mero enteros por teclado.
 	Luego calcular e informar la suma de ellos.
 */
 
@@ -9,6 +9,8 @@ using namespace std;
 int main()
 {
     // < --- (I)
+
+    std::cout << "Hola";
 
     //Declaramos variables
     int num1, num2, resultado;
